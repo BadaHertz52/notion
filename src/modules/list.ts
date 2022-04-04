ba71eb522e980bbcbba0ceebbb03801ddd57e4d0
BadaHertz52 <lmlwpeace@naver.com>
2022-04-04 23:31:40 +0900

    Modify list.ts -edit name of action and add Item type

--- src/modules/list.ts
+++ src/modules/list.ts
@@ -1,140 +1,55 @@
-//ACTION 
-const ADD_LIST ="list/ADD_LIST" as const ;
-const EDIT_LIST ="list/EDIT_LIST" as const ;
-const DELETE_LIST ="list/DELETE_LIST" as const  ; 
-
-const ADD_PAGE ="list/ADD_PAGE" as const ;
-const EDIT_PAGE ="list/EDIT_PAGE" as const ;
-const DELETE_PAGE ="list/DELETE_PAGE" as const ;
-
-export const addList = (id:string ,header:string, data:string) =>({
-  type:ADD_LIST,
-  list :{
-    id: id ,
-    header: header,
-    pages:[]
-  }
-});
-export const editList = (id:string, text : string ) =>({
-  type:EDIT_LIST,
-  list :{
-    id:id,
-    header: text,
-  }
-});
-
-export const deleteList =(id:string )=>({
-  type:DELETE_LIST,
-  list:{
-    id:id,
-  }
-});
-
-export const addPage =(list:List, page:Page)=>({
-  type:ADD_PAGE,
-  list :list,
-  page :page
-});
-
-export const editPage =(list:List, page:Page)=>({
-  type:EDIT_PAGE,
-  list :list,
-  page :page
-});
-export const deletePage =(list:List, pageId:string)=>({
-  type:DELETE_PAGE,
-  list : list,
-  page :{
-    id:pageId,
-  }
-});
-//TYPE 
-type Page ={
-  id:string,
-  header:string ,
-};
-type List ={
+type Item ={
   id:string,
   header:string,
-  pages:Page[]
 };
 
-type ListState =List[];
+type List = Item[];
+
+const ADD_ITEM ="list/ADD_ITEM" as const ;
+const EDIT_ITEM ="list/EDIT_ITEM" as const ;
+const DELETE_ITEM ="list/DELETE_ITEM" as const ;
+
+export const addItem =(item:Item)=>({
+  type:ADD_ITEM,
+  item:item
+});
+
+export const editItem =(item:Item)=>({
+  type:EDIT_ITEM,
+  item:item
+});
+export const deleteItem =(item:Item)=>({
+  type:DELETE_ITEM,
+  item:item
+});
+
 
 type ListAction = 
-ReturnType<typeof addList> | 
-ReturnType<typeof editList> | 
-ReturnType <typeof deleteList>|
-ReturnType <typeof addPage>|
-ReturnType <typeof editPage>|
-ReturnType <typeof deletePage>
+ReturnType <typeof addItem>|
+ReturnType <typeof editItem>|
+ReturnType <typeof deleteItem>
 ;
 
-//REDUCER 
 const initialState =[{
-  id: String(Date.now),
-  header :"welcome notion",
-  pages:[]
+  id:String(Date.now),
+  header:"welcome notion"
 }];
 
-export default function list (state:ListState =initialState, action: ListAction){
+export default function list(state:List =initialState ,action:ListAction){
   switch (action.type) {
-    case ADD_LIST:
-      return state.concat(action.list);
-    case EDIT_LIST :
-      return state.map((list :List)=>{
-        if(list.id === action.list.id){
-          return {...list, header:action.list.header}
-        }else{
-          return list
-        }
-      }) ;
+    case ADD_ITEM:
+      return state.concat(action.item);
 
-    case DELETE_LIST :
-      return state.filter((list:List) => list.id !== action.list.id);
-
-    case ADD_PAGE :
-      return state.map((list:List) => {
-        if( action.list.id === list.id){
-          const newList = {...list, pages:list.pages.concat(action.page)};
-          return newList;
-        }else{
-          return list
-        }
-      });
-
-    case EDIT_PAGE:
-      return state.map((list:List)=>{
-        if(action.list.id === list.id){
-          const newPages = list.pages.map((page:Page)=>{
-            if(page.id=== action.page.id){
-              return {...page, header:action.page.header}
-            }else{
-              return page
-            }
-          });
-
-          return{
-            ...list, pages:newPages
-          }
-        }else{
-          return list
-        }
-      });
-
-    case DELETE_PAGE :
-      return state.map ((list:List)=>{
-        if(action.list.id=== list.id){
-          const newPages = list.pages.filter((page:Page)=> page.id !== action.page.id);
-          return {
-            ...list, pages:newPages
-          }
-        }else{
-          return list
-        }
-      })
+    case EDIT_ITEM:
+      const itemIndexs = state.map(item => item.id);
+      const index = itemIndexs.indexOf(action.item.id);
+      state[index] = action.item ;
+      break;
+    
+    case DELETE_ITEM :
+      return state.filter((item: Item) => item.id !== action.item.id);
+      
     default:
       break;
   }
-};
-
+}
