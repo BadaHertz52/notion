bf36c381abe52ef34ff6797113d09ad5a51edd1a
BadaHertz52 <lmlwpeace@naver.com>
2022-04-05 23:27:01 +0900

    [DOC] Add code to SIdeBar  and Make BlockComponent , Eidtor and Menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Item, List } from '../modules/list';
+import { Notion, Page } from '../modules/notion';
+
+//react-icon
 import {FiCode ,FiChevronsLeft} from 'react-icons/fi';
-import {AiOutlineClockCircle} from 'react-icons/ai';
+import {AiOutlineClockCircle, AiOutlinePlus, AiOutlinePlusSquare} from 'react-icons/ai';
 import {BiSearchAlt2} from 'react-icons/bi';
-import {IoIosSettings} from 'react-icons/io';
-import { List, Page } from '../modules/list';
+import {BsFillTrash2Fill, BsThreeDots} from 'react-icons/bs';
+import {IoIosSettings, IoMdArrowDropright} from 'react-icons/io';
+import {HiDownload, HiTemplate} from 'react-icons/hi';
 
 type SideBarProps ={
   user:{
@@ -12,13 +17,63 @@ type SideBarProps ={
     favorites:string[],
     trash:string[],
   },
-  page:Page,
-  list: List[],
+  list: List,
+  notion:Notion
 };
 
+type ListTemplateProp ={
+  targetList: List
+};
 
-const SideBar =({user ,page, list }:SideBarProps)=>{
+const ListTemplate =({targetList}:ListTemplateProp)=>{
+  return(
+    <ul>
+    {targetList.map((item:Item)=> 
+      <li>
+        <div className='first page'>
+          <button className='toggleBtn'>
+            <IoMdArrowDropright/>
+          </button>
+          <div className='pageName'>
+            {item.icon !==null && 
+              <span>
+                {item.icon}
+              </span>
+              }
+            <span>{item.header}</span>
+          </div>
+          <div className="pageFun">
+            <button  
+              className='dotBtn'
+              title='delete, duplicate, and more'
+            >
+              <BsThreeDots/>
+            </button>
+            <button 
+              className='addPageBtn'
+              title="Quickly add a page inside"
+            >
+              <AiOutlinePlusSquare/>
+            </button>
+          </div>
+        </div>
+        <div className="inside page">
+        </div>
+      </li> 
+    )}
+  </ul>
+  )
+};
+
+const SideBar =({user ,notion, list }:SideBarProps)=>{
   const recordIcon =user.userName.substring(0,1);
+  const favorites:List = notion.pages.filter((page:Page)=> user.favorites.includes(page.id)).map((page:Page)=> ({
+    id:page.id,
+    header:page.header,
+    icon:page.icon
+  }));
+
+
   return(
     <div id="sideBar">
       <div className="switcher">
@@ -51,16 +106,42 @@ const SideBar =({user ,page, list }:SideBarProps)=>{
       <div className="srcoller">
         <div className="favorites">
           <div className="header">
-            FAVORITES
+            FAVORITES 
           </div>
-
+          <div className="list">
+            <ListTemplate targetList={favorites}/>
+          </div>
         </div>
         <div className="private"></div>
           <div className="header">
             PRIVATE
           </div>
+          <div className="list">
+            <ListTemplate targetList={list}/>
+          </div>
       </div>
-      <div className="fun2"></div>
+      <div className="fun2">
+        <ul>
+          <li>
+            <HiTemplate/>
+            <span>Templates</span>
+          </li>
+          <li>
+            <HiDownload/>
+            <span>Import</span>
+          </li>
+          <li>
+            <BsFillTrash2Fill/>
+            <span>Trash</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button>
+          <AiOutlinePlus/>
+          <span>New page</span>
+        </button>
+      </div>
     </div>
   )
 };
