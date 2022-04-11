d2e5a3462f7b7e2931ac0074131399bd8cc24af0
BadaHertz52 <lmlwpeace@naver.com>
2022-04-11 21:49:03 +0900

    [DOC] Transfer prop to EditorContainer from NotionRouter

--- src/containers/NotionRouter.tsx
+++ src/containers/NotionRouter.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../modules';
+import { Page } from '../modules/notion';
 import EditorContainer from './EditorContainer';
 import SideBarContainer from './SideBarContainer';
 
 const NotionRouter =()=>{
+  const notion = useSelector((state:RootState)=> state.notion);
+  const firstPage = notion.pages[0];
+  const [targetPage, setTargetPage]= useState<Page>(firstPage);
+
   return(
     <div id="inner">
-      <SideBarContainer/>
-      <EditorContainer/>
+      <SideBarContainer />
+      <EditorContainer page={targetPage}/>
     </div>
   )
 };
