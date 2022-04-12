17dc423e6d42a6cb0846ea257998975cd43d70cb
BadaHertz52 <lmlwpeace@naver.com>
2022-04-12 21:05:05 +0900

    [DOC] Get new props which are newPage and setNewPage from NotionRouter

--- src/containers/EditorContainer.tsx
+++ src/containers/EditorContainer.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Editor from '../components/Editor';
 import { RootState } from '../modules';
-import { Block, editBlock, Page } from '../modules/notion';
+import { Block, editBlock, Notion, Page } from '../modules/notion';
 
 type EditorContainerProps ={
-  page: Page
+  page: Page,
+  pagePath : string [] | null,
+  newPage:boolean,
+  setNewPage:Dispatch<SetStateAction<boolean>>
 }
-const EditorContainer =({page}:EditorContainerProps)=>{
+const EditorContainer =({page , pagePath , newPage, setNewPage}:EditorContainerProps)=>{
   const dispatch =useDispatch();
   const editBlock_action = (pageId:string, block:Block)=> {dispatch(editBlock(pageId, block))};
 
   return(
     <Editor 
       page={page}
+      pagePath ={pagePath}
       editBlock ={editBlock_action}
+      newPage ={newPage}
+      setNewPage ={setNewPage}
     />
   )
 };
