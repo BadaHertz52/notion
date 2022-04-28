11c91cbee17010265952198c1056956c2322379c
BadaHertz52 <lmlwpeace@naver.com>
2022-04-28 22:32:57 +0900

    [DOC] Delete unnessary and Change const to function

--- src/components/EditableBlock.tsx
+++ src/components/EditableBlock.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useRef, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 import { Block, Page } from '../modules/notion';
@@ -8,34 +8,44 @@ type EditableBlockProps ={
   page:Page,
   //element: JSX.Element,
   block:Block,
-  setBlocks:Dispatch<SetStateAction<Block[]>>,
   editBlock :(pageId:string, block:Block)=>void,
   addBlock :(pageId:string, block:Block , nextBlockIndex:number)=>void,
   changeToSub :(pageId:string, block:Block)=>void,
   // raiseBlock: (pageId:string, block:Block)=>void,
 }
-const EditableBlock =({page, block  ,setBlocks ,editBlock ,addBlock, changeToSub}:EditableBlockProps)=>{
+const EditableBlock =({page, block   ,editBlock ,addBlock, changeToSub}:EditableBlockProps)=>{  
   const blockIndex:number = page.blocksId.indexOf(block.id) ;
   const nextBlockIndex :number= blockIndex +1; 
   const  editTime = JSON.stringify(Date.now());
   const innerRef  =useRef<HTMLDivElement>(null) ;
   const [targetBlock, setTargetBlock]=useState<Block>(block); 
-  const [sub, setSub]=useState<boolean>(false);
+  const [newBlock, setNewBlock]=useState<boolean>(false);
+  const [newBlockDoc, setNewBlockDoc]=useState<HTMLElement| null>(null);
+ 
+  useEffect(()=>{
+    newBlockDoc?.focus();
+  },[newBlockDoc , newBlock]);
 
-  const callBlockNode=(block:Block):string=>{
-    const blockNode = ReactDOMServer.renderToString(<BlockComponent page={page} block={block}/>);
+  function callBlockNode(block:Block):string{
+    const blockNode = ReactDOMServer.renderToString(<BlockComponent block={block}/>);
     return blockNode
   };
-    const editContents =(contents :string , block:Block )=> {
-    const newBlock :Block ={
-      ...block,
-      contents: contents,
-      editTime: editTime
-    } ;
-    editBlock(page.id,newBlock);
-    setBlocks(page.blocks);
+
+  function editContents(contents :string , block:Block ){
+  const newBlock :Block ={
+    ...block,
+    contents: contents,
+    editTime: editTime
+  } ;
+  editBlock(page.id,newBlock);
   };
-  const onChange =(event:ContentEditableEvent)=>{   
+
+  function addNewBlock( newBlock:Block, newBlockIndex:number){
+    addBlock(page.id,newBlock,newBlockIndex);
+    setNewBlockDoc(document.getElementById(newBlock.id));
+  };
+
+  function onChange(event:ContentEditableEvent){   
     const value = event.target.value;
       
       if(!value.includes("<div>")){
@@ -50,7 +60,7 @@ const EditableBlock =({page, block  ,setBlocks ,editBlock ,addBlock, changeToSub
       };
   };
 
-  const make_subBlock =(parentBlock:Block, subBlock:Block)=>{
+  function make_subBlock(parentBlock:Block, subBlock:Block){
       const newMainBlock:Block ={
         ...parentBlock, 
         editTime :editTime ,
@@ -62,60 +72,57 @@ const EditableBlock =({page, block  ,setBlocks ,editBlock ,addBlock, changeToSub
           parentBlock.id
         ]
       };
-      addBlock(page.id,newSubBlock,nextBlockIndex);
+      addNewBlock(newSubBlock,nextBlockIndex);
       editBlock(page.id, newMainBlock);
-      document.getElementById(newSubBlock.id)?.focus();
-    };
-  
-    const onKeydown =(event: React.KeyboardEvent<HTMLDivElement>)=>{
-      if(event.code === "Enter"){
-        // 새로운 블록 만들기 
-          const newBlock:Block ={
-            id:editTime,
-            editTime:editTime,
-            type:"text",
-            contents:"",
-            firstBlock:true,
-            subBlocksId:null,
-            parentBlocksId:null,
-            icon:null,
-          };
-      
-        if(block.type.includes("toggle")){
-          //subBtn 
-          const newSubBlock:Block ={
-            ...newBlock,
-            firstBlock:false,
-            parentBlocksId:[targetBlock.id]
-          }
-          make_subBlock(targetBlock, newSubBlock);
-          
-        }else{
-          //새로운 버튼 
-          addBlock(page.id, newBlock, nextBlockIndex);
-          document.getElementById(newBlock.id)?.focus();
-        }
-      } ;
-      if(event.code ==="Tab" && blockIndex>0){
-        //  이전 블록의 sub 으로 변경 
-        innerRef.current?.focus();
-        const newParentBlock:Block = page.blocks[blockIndex-1];
-        const editedBlock:Block ={
-          ...targetBlock,
-          firstBlock: false,
-          parentBlocksId: newParentBlock.parentBlocksId? newParentBlock.parentBlocksId.concat(newParentBlock.id) : [newParentBlock.id],
-          editTime:editTime
-        };
-        changeToSub(page.id, editedBlock);
       };
-      console.log("pagebLOCKS", page.blocks)
-      setBlocks(page.blocks);
+  
+  function onKeydown (event: React.KeyboardEvent<HTMLDivElement>){
+    if(event.code === "Enter"){
+      // 새로운 블록 만들기 
+        const newBlock:Block ={
+          id:editTime,
+          editTime:editTime,
+          type:"text",
+          contents:"",
+          firstBlock:true,
+          subBlocksId:null,
+          parentBlocksId:null,
+          icon:null,
+        };
+    
+      if(block.type.includes("toggle")){
+        //subBtn 
+        const newSubBlock:Block ={
+          ...newBlock,
+          firstBlock:false,
+          parentBlocksId:[targetBlock.id]
+        }
+        make_subBlock(targetBlock, newSubBlock);
+        
+      }else{
+        //새로운 버튼 
+        addNewBlock(newBlock, nextBlockIndex);
+      }
+    } ;
+    if(event.code ==="Tab" && blockIndex>0){
+      //  이전 블록의 sub 으로 변경 
+      innerRef.current?.focus();
+      const newParentBlock:Block = page.blocks[blockIndex-1];
+      const editedBlock:Block ={
+        ...targetBlock,
+        firstBlock: false,
+        parentBlocksId: newParentBlock.parentBlocksId? newParentBlock.parentBlocksId.concat(newParentBlock.id) : [newParentBlock.id],
+        editTime:editTime
+      };
+      changeToSub(page.id, editedBlock);
     };
+  };
+
   return(
     <ContentEditable
-      id={targetBlock.id}
+      id={block.id}
       className="editableBlock"
-      html={callBlockNode(targetBlock)}
+      html={callBlockNode(block)}
       innerRef={innerRef}
       onChange={onChange}
       onKeyDown={onKeydown}
