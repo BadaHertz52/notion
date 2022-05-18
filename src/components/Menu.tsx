21a8761e6941754e879e7a94a4b2b64d753d4099
BadaHertz52 <lmlwpeace@naver.com>
2022-05-18 18:47:48 +0900

    [DOC] Delete Unnecessary

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../modules';
 import { Block} from '../modules/notion';
 //icon
 import {BiCommentDetail} from 'react-icons/bi';
