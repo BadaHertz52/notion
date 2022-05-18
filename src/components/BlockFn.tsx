d54ffea1c9c616dafdc08b3b7ae1bd55908876ec
BadaHertz52 <lmlwpeace@naver.com>
2022-05-18 18:43:26 +0900

    [FEAT] Make makeBlock function and Use it when user click addBlock icon in BlockFn

--- src/components/BlockFn.tsx
+++ src/components/BlockFn.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './Menu';
-import {Block} from '../modules/notion';
+import {Block, blockSample, Page} from '../modules/notion';
 
 import { AiOutlinePlus } from 'react-icons/ai';
 import { CgMenuGridO } from 'react-icons/cg';
+import { makeNewBlock } from './EditableBlock';
 
 type BlockFnProp ={
-  block: Block,
+  page:Page,
   userName:string,
+  addBlock: (pageId: string, block: Block, nextBlockIndex: number, previousBlockId: string | null) => void, 
+  editBlock: (pageId: string, block: Block) => void
 };
 
-const BlockFn =({block ,userName}:BlockFnProp)=>{
+const BlockFn =({page,userName, addBlock, editBlock}:BlockFnProp)=>{
+  const editTime = JSON.stringify(Date.now);
+  const newContents:string ="";
+  const [block, setBlock]=useState<Block>(blockSample);
+
+  const makeBlock =()=>{
+    const sessionItem = sessionStorage.getItem("blockFnTargetBlock") as string;
+    const targetBlock= JSON.parse(sessionItem);
+    setBlock(targetBlock);
+    const targetBlockIndex= page.blocksId.indexOf(targetBlock.id);
+    makeNewBlock(page, editTime,addBlock, editBlock, targetBlock, targetBlockIndex, newContents);
+  };
+
   return (
     <div 
       id="blockFn"
@@ -19,6 +34,7 @@ const BlockFn =({block ,userName}:BlockFnProp)=>{
       <button 
         className='addBlock'
         title="Click  to add a block below"
+        onClick={makeBlock}
       >
         <AiOutlinePlus/>
       </button>
