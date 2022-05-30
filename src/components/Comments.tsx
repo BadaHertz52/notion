f899b1870a8ea9f37d70af5b5b5fb73ae58b8b67
BadaHertz52 <lmlwpeace@naver.com>
2022-05-30 14:51:39 +0900

    [FEAT] Make CommentInput and type of it

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
-import { CommentType } from '../modules/notion';
+import React, { FormEvent, FormEventHandler, useState } from 'react';
+import { BsFillArrowUpCircleFill } from 'react-icons/bs';
+import { Block, CommentType, page } from '../modules/notion';
 type CommentComponentProps={
-  comments:CommentType[]
+  pageId: string,
+  block:Block,
+  userName:string,
+  editBlock :(pageId: string, block: Block) => void,
 };
 type CommentProps ={
   comment:CommentType
+};
+type CommentInputProps={
+  userName: string,
+  pageId: string,
+  block:Block,
+  editBlock :(pageId: string, block: Block) => void,
 }
-const Comments =({comments}:CommentComponentProps)=>{
+export const CommentInput =({userName, pageId, block ,editBlock}:CommentInputProps)=>{
+  const userNameFirstLetter =userName.substring(0,1).toUpperCase();
+  const inputSubmit = document.getElementById("commentInputSubmit") as HTMLInputElement;
+  const [text, setText]=useState<string>("");
+
+  const onInputText=(event:FormEvent)=>{
+    const target =event?.target as Node; 
+    const value =target.textContent;
+  };
+  const addComment =()=>{
+    const value = inputSubmit.value; 
+    const newComment :CommentType ={
+      userName:userName,
+      comment:value,
+      editTime:JSON.stringify(Date.now())
+    };
+    const newBlock ={
+      ...block,
+      comment:block.comments === null? [newComment]:block.comments.concat(newComment)
+    };
+    editBlock(pageId ,newBlock );
+  };
+
+  return(
+    <div className="commentInput">
+      <div className='firstLetter'>
+        {userNameFirstLetter}
+      </div>
+      <div className='commentInput_form'>
+        <form
+          onSubmit={addComment}
+        >
+          <input
+            type="text"
+            placeholder='Add a comment'
+            name="comment"
+            value={text}
+            onInput={onInputText}
+          />
+
+          <input 
+            type="submit"
+            id="commentInPutSubmit"
+            value="submit"
+          >
+          </input>
+        </form>
+      </div>
+    </div>
+  )
+};
+const Comments =({pageId,block, userName ,editBlock}:CommentComponentProps)=>{
+  const comments=block.comments ;
 
   const Comment =({comment}:CommentProps)=>{
     const firstLetter = comment.userName.substring(0,1).toUpperCase();
@@ -22,12 +84,18 @@ const Comments =({comments}:CommentComponentProps)=>{
   }
   return(
     <div className='comments'>
-      {comments.map((comment:CommentType)=>
+      {comments?.map((comment:CommentType)=>
         <Comment 
           key={`comment_${comments.indexOf(comment)}`}
           comment={comment} 
         />
       )}
+      <CommentInput
+        userName={userName}
+        block={block}
+        pageId={pageId}
+        editBlock={editBlock}
+      />
     </div>
   )
 };
