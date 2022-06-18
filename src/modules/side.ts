88e7c4063187aea4e3e190e9515da6f08edafce5
BadaHertz52 <lmlwpeace@naver.com>
2022-06-18 18:22:10 +0900

    [DOC] Change action state name that was left  to  float

--- src/modules/side.ts
+++ src/modules/side.ts
@@ -1,7 +1,7 @@
 const lock ="lock" as const ;
-const left ="left" as const ;
+const float ="float" as const ;
 const close ="close" as const; 
-export type SideAppear = typeof lock | typeof left | typeof close
+export type SideAppear = typeof lock | typeof float | typeof close
 
 const CHANGE_SIDE = 'side/CHANGE_SIDE' as const ;
 export const change_side =(appear:SideAppear)=>({
@@ -15,12 +15,18 @@ type SideAction =
 ReturnType<typeof change_side> 
 
 const initialState:Side = {
-  appear:lock
+  appear:float
 } ;
 
 export default function side (state:Side = initialState, action:SideAction):Side{
   switch (action.type) {
     case CHANGE_SIDE:
+      const sideBar =document.getElementById("inner");
+      if(sideBar !== null){
+        const classList =sideBar.classList;
+        const newClassName =`sideBar_${action.appear}`
+        sideBar.classList.replace(classList[0], newClassName);
+      }
       return  { appear: action.appear} ;
     default:
       return state;
