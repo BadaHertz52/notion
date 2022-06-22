eba5c44e7acd717543f4730999a7c5e3f01355cf
BadaHertz52 <lmlwpeace@naver.com>
2022-06-22 23:04:22 +0900

    [DOC] Refactory Editable and BlockComponet to move return value of BlockCompoent to Editable

--- src/components/BlockComponent.tsx
+++ src/components/BlockComponent.tsx
@@ -1,15 +1,12 @@
-import React, { Dispatch, SetStateAction, useRef} from 'react';
+import React, { Dispatch, SetStateAction, useRef, useState} from 'react';
 import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
-import { GoPrimitiveDot } from 'react-icons/go';
-import { GrCheckbox, GrCheckboxSelected, GrDocumentText } from 'react-icons/gr';
 import { IoChatboxOutline } from 'react-icons/io5';
-import { MdPlayArrow } from 'react-icons/md';
-import {  CSSProperties} from 'styled-components';
-import EditorContainer, { Command } from '../containers/EditorContainer';
-import {  Block,BlockType,blockTypes,findBlock,findParentBlock,makeNewBlock,Page, toggle } from '../modules/notion';
-import EditableBlock from './EditableBlock';
 
-type BlockProp ={
+import { Command } from '../containers/EditorContainer';
+import {  Block,BlockType,blockTypes,findBlock,makeNewBlock,Page, toggle } from '../modules/notion';
+import CommandBlock from './CommandBlock';
+
+type  BlockProps ={
   userName:string,
   block:Block,
   page:Page,
@@ -24,17 +21,10 @@ type BlockProp ={
   setCommentBlock : Dispatch<SetStateAction<Block|null>>,
   commentBlock:Block|null,
   setTargetPageId:Dispatch<SetStateAction<string>>,
-  setCommand: Dispatch<SetStateAction<Command>>,
-  command:Command,
 };
-type BlockCommentProps ={
-  block:Block,
-};
-type BlockContentProps={
-  block:Block,
-  onChangeContents: (event: ContentEditableEvent) => void,
-  onKeyDownContents: (event: React.KeyboardEvent<HTMLDivElement>) => void,
-};
+type BlockCommentProps={
+  block:Block
+}
 export type itemType ={
   block:Block,
   blockIndex:number ,
@@ -58,9 +48,19 @@ export const BlockComment =({block}:BlockCommentProps)=>{
 
   )
 };
-const BlockContent =({block, onChangeContents, onKeyDownContents}:BlockContentProps)=>{
-  const contentEditableRef= useRef<HTMLElement>(null);
 
+const BlockComponent=({ userName,block, page ,addBlock,editBlock,changeToSub,raiseBlock, deleteBlock ,addPage, editPage, deletePage,setCommentBlock ,commentBlock ,setTargetPageId  }:BlockProps)=>{
+  const editTime =JSON.stringify(Date.now);
+  const contentEditableRef= useRef<HTMLElement>(null);
+  const [command, setCommand]=useState<Command>({boolean:false, command:null})
+  const findTargetBlock =(event:ContentEditableEvent|React.KeyboardEvent<HTMLDivElement>|React.MouseEvent):Block=>{
+    const target =event.currentTarget.parentElement as HTMLElement;
+    const targetId= target.id;
+    const end =targetId.indexOf("_contents");
+    const blockId = targetId.slice(0, end);
+    const targetBlock = findBlock(page, blockId).BLOCK;
+    return targetBlock;
+  };
   const showBlockFn=(event: React.MouseEvent)=>{
     const currentTarget =event.currentTarget as Element;
     const mainBlock= currentTarget.parentElement?.parentElement ;
@@ -80,136 +80,61 @@ const BlockContent =({block, onChangeContents, onKeyDownContents}:BlockContentPr
       blockFn?.setAttribute("style",blockFnStyle);
     }
   };
-
-  const BlockContentEditable=()=>{
-
-    return(
-        <ContentEditable
-          className='contentEditable'
-          placeholder="type '/' for commmands"
-          html= {block.contents}
-          innerRef={contentEditableRef}
-          onChange={(event)=> onChangeContents(event )}
-          onKeyDown={(event)=> onKeyDownContents(event)}
-        /> 
-    )
-  };
-  return(
-    <>
-    {block.comments ==null ?
-    ( block.type==="page"?
-      <button 
-        className="contents pageTitle"
-        id={`${block.id}_contents`}
-        onMouseOver={showBlockFn}
-      >
-        <BlockContentEditable/>
-      </button>
-      :
-      <div 
-        id={`${block.id}_contents`}
-        className="contents"
-        onMouseOver={showBlockFn}
-      >
-        <BlockContentEditable/>
-      </div>
-    )
-    :
-    <button 
-      id={`${block.id}_contents`}
-      className="contents commentBtn"
-      onMouseOver={showBlockFn}
-    >
-      <BlockContentEditable/>
-    </button>
-    }
-  </>
-  )
-};
-const BlockComponent=({ userName,block, page ,addBlock,editBlock,changeToSub,raiseBlock, deleteBlock ,addPage, editPage, deletePage,setCommentBlock ,commentBlock ,setTargetPageId ,setCommand , command }:BlockProp)=>{
-  const className = block.type !== "toggle" ?
-                    `${block.type} block ` :
-                    `${block.type} block ${block.subBlocksId!==null?'on' : ""}`;
-  const editTime = JSON.stringify(Date.now());
-  const subBlocks =  block.subBlocksId?.map((id:string)=>findBlock(page, id).BLOCK)
-  //const [text, setText] =useState<string>(block.contents);
-  const blockContentsStyle =(block:Block):CSSProperties =>{
-    return ({
-      color: block.type !=="todo done" ? block.style.color: "grey",
-      backgroundColor :block.style.bgColor,
-      fontWeight: block.style.fontWeight ,
-      fontStyle: block.type !=="todo done"? block.style.fontStyle : "italic",
-      textDecoration: block.type !=="todo done"? block.style.textDeco :"line-through",
-    })
-  };
-  const findTargetBlock =(event:ContentEditableEvent|React.KeyboardEvent<HTMLDivElement>|React.MouseEvent):Block=>{
-    const target =event.currentTarget.parentElement as HTMLElement;
-    const targetId= target.id;
-    const end =targetId.indexOf("_contents");
-    const blockId = targetId.slice(0, end);
-    const targetBlock = findBlock(page, blockId).BLOCK;
-    return targetBlock;
-  };
-  const giveFocusToContent =(event:React.MouseEvent)=>{
-    const currentTarget =event.currentTarget as HTMLElement;
-    const contentEditable =currentTarget.getElementsByClassName("contentEditable")[0] as HTMLElement ;
-    console.log(currentTarget, contentEditable);
-    contentEditable.focus();
-  }
   const onChangeContents=(event:ContentEditableEvent)=>{
     const value =event.target.value;
     const targetBlock= findTargetBlock(event);
     const targetBlockIndex= page.blocksId.indexOf(targetBlock.id);
-    if(value.includes("<div>")){
-      //enter 시에 새로운 블록 생성 
-      const start = value.indexOf("<div>");
-      const end =value.indexOf("</div>");
-      const editedContents =value.slice(0, start);
-      const newBlockContents= value.slice(start+5,end );
-      const newBlock:Block ={
-        ...makeNewBlock(page, targetBlock, newBlockContents),
-        firstBlock: targetBlock.firstBlock,
-        subBlocksId: targetBlock.subBlocksId
-      } ;
-      const previouseBlockId : string|null = targetBlock.firstBlock ? null : targetBlock.id ;
-
-      if((targetBlock.contents!== editedContents) || (targetBlock.subBlocksId!==null)){
-        const editedBlock:Block ={
-          ...targetBlock,
-          contents:block.contents!== editedContents ?editedContents : targetBlock.contents,
-          subBlocksId: targetBlock.subBlocksId !==null ? null : targetBlock.subBlocksId,
-          editTime:editTime,
+    const changeBlockContent =()=>{
+      if(value.includes("<div>")){
+        //enter 시에 새로운 블록 생성 
+        const start = value.indexOf("<div>");
+        const end =value.indexOf("</div>");
+        const editedContents =value.slice(0, start);
+        const newBlockContents= value.slice(start+5,end );
+        const newBlock:Block ={
+          ...makeNewBlock(page, targetBlock, newBlockContents),
+          firstBlock: targetBlock.firstBlock,
+          subBlocksId: targetBlock.subBlocksId
+        } ;
+        const previouseBlockId : string|null = targetBlock.firstBlock ? null : targetBlock.id ;
+  
+        if((targetBlock.contents!== editedContents) || (targetBlock.subBlocksId!==null)){
+          const editedBlock:Block ={
+            ...targetBlock,
+            contents:block.contents!== editedContents ?editedContents : targetBlock.contents,
+            subBlocksId: targetBlock.subBlocksId !==null ? null : targetBlock.subBlocksId,
+            editTime:editTime,
+          };
+          editBlock(page.id, editedBlock);
+        }
+        if(block.type ===toggle){
+          const newSubToggleBlock :Block ={
+            ...newBlock,
+            parentBlocksId:[targetBlock.id],
+            firstBlock:false,
+          };
+          addBlock(page.id, newSubToggleBlock, targetBlockIndex+1, previouseBlockId);
+  
+        }else{
+          addBlock(page.id, newBlock,targetBlockIndex+1, previouseBlockId);
         };
-        editBlock(page.id, editedBlock);
-      }
-      if(block.type ===toggle){
-        const newSubToggleBlock :Block ={
-          ...newBlock,
-          parentBlocksId:[targetBlock.id],
-          firstBlock:false,
-        };
-        addBlock(page.id, newSubToggleBlock, targetBlockIndex+1, previouseBlockId);
-
       }else{
-        addBlock(page.id, newBlock,targetBlockIndex+1, previouseBlockId);
+        // edite targetBlock 
+        
+        const editedBlock :Block ={
+                  ...targetBlock,
+                  contents: value,
+                  editTime:editTime,
+                };
+        targetBlock.contents !== value &&
+        sessionStorage.setItem("itemsTobeEdited", JSON.stringify(editedBlock));
       };
+    }
+    if(!value.startsWith("/")){
+      changeBlockContent();
     }else{
-      // edite targetBlock 
       
-      const editedBlock :Block ={
-                ...targetBlock,
-                contents: value,
-                editTime:editTime,
-              };
-      targetBlock.contents !== value &&
-      sessionStorage.setItem("itemsTobeEdited", JSON.stringify(editedBlock));
-    };
-    /// about Commane
-    if(value.startsWith("/")){
-      setCommand({boolean:true, command:value})
-    }else{
-      command.boolean &&
-      setCommand({boolean:false, command:null})
+      commandChange(event);
     }
   };
   const onKeyDownContents=(event:React.KeyboardEvent<HTMLDivElement>)=>{
@@ -251,7 +176,6 @@ const BlockComponent=({ userName,block, page ,addBlock,editBlock,changeToSub,rai
     };
 
   };
-
   function commandKeyUp(event:React.KeyboardEvent<HTMLDivElement>, block:Block){
     const code= event.code;
     const firstOn =document.querySelector(".command_btn.on.first");
@@ -269,166 +193,62 @@ const BlockComponent=({ userName,block, page ,addBlock,editBlock,changeToSub,rai
       setCommand({boolean:false, command:null})
     }
   };
-  
-  const ListSub = ()=>{
-    const blockContentsRef= useRef<HTMLDivElement>(null);
-    const listStyle =(block:Block):CSSProperties=>{
-      return({
-        textDecoration:"none",
-        fontStyle:"normal",
-        fontWeight: "normal",
-        backgroundColor:block.style.bgColor,
-        color:block.style.color
-      })
-    }
+  const BlockContentEditable=()=>{
     return(
       <>
-        {subBlocks !== undefined  && 
-          subBlocks.map((block:Block)=>(
-          <div 
-            className='list mainBlock'
-            key={`listItem_${subBlocks.indexOf(block)}`}
-          >
-            <div className='mainBlock_block'>
-            <div 
-              id ={block.id}
-              className= "blockContents"
-              ref={blockContentsRef}
-              style={listStyle(block)}
-              onMouseOver={giveFocusToContent}
-              >
-              <div 
-                className='list_marker'
-              >
-                {className.includes("number")? 
-                `${subBlocks.indexOf(block)+1}.`
-                :
-                <GoPrimitiveDot/> 
-                }
-              </div>
-              <BlockContent 
-                block={block}
-                onChangeContents={onChangeContents}
-                onKeyDownContents={onKeyDownContents}
-              />
-            </div>
-            </div>
-          {block.comments !==null &&
-            <BlockComment
+        <ContentEditable
+          className='contentEditable'
+          placeholder="type '/' for commmands"
+          html= {block.contents}
+          innerRef={contentEditableRef}
+          onChange={(event)=> onChangeContents(event )}
+          onKeyDown={(event)=> onKeyDownContents(event)}
+        /> 
+        {command.boolean &&
+          <CommandBlock 
+            key={`${block.id}_command`}
+            page={page}
             block={block}
-            />
-          }
-          </div>
-        ))
+            editTime={editTime}
+            editBlock={editBlock}
+            command={command}
+            setCommand={setCommand}
+            addPage={addPage}
+          />
         }
       </>
     )
   };
-
   return(
-    <div 
-      id={`block_${block.id}`}
-      className={className} 
-    > 
-
-      {(block.type ==="numberList" || block.type=== "bulletList" ) ?
-        <ListSub/>
-      :
-      <>
-      <div 
-        className="mainBlock"
-      >
-        <div className='mainBlock_block'>
-        {block.type ==="todo" &&
-          <button 
-            className='checkbox left blockBtn'
-            name={block.id}
-            >
-            <GrCheckbox 
-              className='blockBtnSvg' 
-            />
-          </button>
-        }
-        {block.type ==="todo done" &&
-          <button 
-            className='checkbox done left blockBtn'
-            name={block.id}
-          >
-            <GrCheckboxSelected   className='blockBtnSvg '
-            />
-          </button>
-        }
-        {block.type ==="toggle" &&
-          <button 
-            name={block.id}
-            className={block.subBlocksId!==null ? 
-              'blockToggleBtn on left blockBtn' :
-              'blockToggleBtn left blockBtn' 
-            }
-          >
-            <MdPlayArrow 
-              className='blockBtnSvg'
-            />
-          </button>
-        }
-        {block.type ==="page" &&
-          <div 
-            className='pageIcon left'
-          >
-          {block.icon == null?
-            < GrDocumentText/>
-          :
-            block.icon
-          }
-          </div>
-        }
-        <div 
-          className='blockContents' 
-          style={blockContentsStyle(block)}
-          onMouseOver={ giveFocusToContent}
+    <>
+      {block.comments ==null ?
+      ( block.type==="page"?
+        <button 
+          className="contents pageTitle"
+          id={`${block.id}_contents`}
+          onMouseOver={showBlockFn}
         >
-        <BlockContent
-          block={block}
-          onChangeContents={onChangeContents}
-          onKeyDownContents={onKeyDownContents}
-        />
+          <BlockContentEditable/>
+        </button>
+        :
+        <div 
+          id={`${block.id}_contents`}
+          className="contents"
+          onMouseOver={showBlockFn}
+        >
+          <BlockContentEditable/>
         </div>
-        </div>
-        {block.comments !==null &&
-        <BlockComment
-          block={block}
-        />
-        }
-      </div>
-      </>
-      }
-      <div 
-        className='subBlocks'
+      )
+      :
+      <button 
+        id={`${block.id}_contents`}
+        className="contents commentBtn"
+        onMouseOver={showBlockFn}
       >
-        {subBlocks!==undefined&&
-        subBlocks.map((subBlock :Block)=> 
-          <EditableBlock
-            key ={subBlocks.indexOf(subBlock)} 
-            userName={userName} 
-            page={page}
-            block={subBlock}
-            addBlock={addBlock}
-            editBlock={editBlock}
-            changeToSub={changeToSub}
-            raiseBlock={raiseBlock}
-            deleteBlock={deleteBlock}
-            addPage={addPage}
-            editPage={editPage}
-            deletePage={deletePage}
-            setCommentBlock={setCommentBlock}
-            commentBlock={commentBlock}
-            setTargetPageId={setTargetPageId}
-          />
-        )
-        }
-      </div>
-
-    </div>
+        <BlockContentEditable/>
+      </button>
+      }
+    </>
   )
 };
 
--- src/components/EditableBlock.tsx
+++ src/components/EditableBlock.tsx
@@ -1,8 +1,11 @@
-import React, { Dispatch, SetStateAction, useEffect, useState, } from 'react';
-import { Block, Page,  } from '../modules/notion';
+import React, { CSSProperties, Dispatch, SetStateAction, useEffect, useRef, useState, } from 'react';
+import { Block, findBlock, Page,  } from '../modules/notion';
 import CommandBlock from './CommandBlock';
 import { Command } from '../containers/EditorContainer';
-import BlockComponent, { itemType } from './BlockComponent';
+import BlockComponent, { BlockComment, itemType } from './BlockComponent';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { GrCheckbox, GrCheckboxSelected, GrDocumentText } from 'react-icons/gr';
+import { MdPlayArrow } from 'react-icons/md';
 
 
 type EditableBlockProps ={
@@ -26,12 +29,29 @@ export   type CommentOpenType ={
   open:boolean,
   targetId: string | null,
 };
+
+
 const EditableBlock =({userName, page, block , editBlock, addBlock,changeToSub ,raiseBlock, deleteBlock , addPage, editPage, deletePage ,setCommentBlock ,commentBlock ,setTargetPageId}:EditableBlockProps)=>{  
-  const  editTime = JSON.stringify(Date.now());
-  const [command, setCommand] =useState<Command>({
-    boolean:false,
-    command:null
-  }); 
+  const className = block.type !== "toggle" ?
+  `${block.type} block ` :
+  `${block.type} block ${block.subBlocksId!==null?'on' : ""}`;
+  const subBlocks =  block.subBlocksId?.map((id:string)=>findBlock(page, id).BLOCK)
+  const blockContentsStyle =(block:Block):CSSProperties =>{
+  return ({
+  color: block.type !=="todo done" ? block.style.color: "grey",
+  backgroundColor :block.style.bgColor,
+  fontWeight: block.style.fontWeight ,
+  fontStyle: block.type !=="todo done"? block.style.fontStyle : "italic",
+  textDecoration: block.type !=="todo done"? block.style.textDeco :"line-through",
+  })
+  };
+  const giveFocusToContent =(event:React.MouseEvent)=>{
+  const currentTarget =event.currentTarget as HTMLElement;
+  const contentEditable =currentTarget.getElementsByClassName("contentEditable")[0] as HTMLElement ;
+  console.log(currentTarget, contentEditable);
+  contentEditable.focus();
+  }
+
   const updateBlock=()=>{
     const item = sessionStorage.getItem("itemsTobeEdited");
     const cursorElement =document.getSelection()?.anchorNode?.parentElement;
@@ -58,66 +78,190 @@ const EditableBlock =({userName, page, block , editBlock, addBlock,changeToSub ,
         };
         sessionStorage.removeItem("newBlock");
     }
-  },[])
-  return(
-      <div 
-        className="editableBlock"
-        
-      >
-        <div className='editableBlockInner'>
-        {!command.boolean?
-        <>
-          <BlockComponent
-            userName={userName} 
-            block={block} 
-            page={page}
-            addBlock={addBlock}
-            editBlock={editBlock}
-            changeToSub={changeToSub}
-            raiseBlock={raiseBlock}
-            deleteBlock={deleteBlock}
-            addPage={addPage}
-            editPage={editPage}
-            deletePage={deletePage}
-            setCommentBlock={setCommentBlock}
-            commentBlock={commentBlock}
-            setTargetPageId={setTargetPageId}
-            command={command}
-            setCommand={setCommand}    
-          />
-        </>
-        :
-          <>
-          <BlockComponent
-            userName={userName} 
-            block={block} 
-            page={page}
-            addBlock={addBlock}
-            editBlock={editBlock}
-            changeToSub={changeToSub}
-            raiseBlock={raiseBlock}
-            deleteBlock={deleteBlock}
-            addPage={addPage}
-            editPage={editPage}
-            deletePage={deletePage}
-            setCommentBlock={setCommentBlock}
-            commentBlock={commentBlock}
-            setTargetPageId={setTargetPageId}
-            command={command}
-            setCommand={setCommand}
-          />
-          <CommandBlock 
-            key={`${block.id}_command`}
-            page={page}
-            block={block}
-            editTime={editTime}
-            editBlock={editBlock}
-            command={command}
-            setCommand={setCommand}
-            addPage={addPage}
-          />
-          </>
+  },[]);
+  
+  const ListSub = ()=>{
+    const blockContentsRef= useRef<HTMLDivElement>(null);
+    const listStyle =(block:Block):CSSProperties=>{
+      return({
+        textDecoration:"none",
+        fontStyle:"normal",
+        fontWeight: "normal",
+        backgroundColor:block.style.bgColor,
+        color:block.style.color
+      })
+    };
+    return(
+      <>
+        {subBlocks !== undefined  && 
+          subBlocks.map((block:Block)=>(
+          <div 
+            className='list mainBlock'
+            key={`listItem_${subBlocks.indexOf(block)}`}
+          >
+            <div className='mainBlock_block'>
+            <div 
+              id ={block.id}
+              className= "blockContents"
+              ref={blockContentsRef}
+              style={listStyle(block)}
+              onMouseOver={giveFocusToContent}
+              >
+              <div 
+                className='list_marker'
+              >
+                {className.includes("number")? 
+                `${subBlocks.indexOf(block)+1}.`
+                :
+                <GoPrimitiveDot/> 
+                }
+              </div>
+              <BlockComponent 
+                userName={userName} 
+                block={block} 
+                page={page}
+                addBlock={addBlock}
+                editBlock={editBlock}
+                changeToSub={changeToSub}
+                raiseBlock={raiseBlock}
+                deleteBlock={deleteBlock}
+                addPage={addPage}
+                editPage={editPage}
+                deletePage={deletePage}
+                setCommentBlock={setCommentBlock}
+                commentBlock={commentBlock}
+                setTargetPageId={setTargetPageId}
+              />
+            </div>
+            </div>
+          {block.comments !==null &&
+            <BlockComment
+              block={block} 
+            />
+          }
+          </div>
+        ))
         }
+      </>
+    )
+  };
+  return(
+      <div className="editableBlock">
+        <div className='editableBlockInner'>
+          <div 
+            id={`block_${block.id}`}
+            className={className} 
+          > 
+
+            {(block.type ==="numberList" || block.type=== "bulletList" ) ?
+              <ListSub/>
+            :
+            <>
+            <div 
+              className="mainBlock"
+            >
+              <div className='mainBlock_block'>
+              {block.type ==="todo" &&
+                <button 
+                  className='checkbox left blockBtn'
+                  name={block.id}
+                  >
+                  <GrCheckbox 
+                    className='blockBtnSvg' 
+                  />
+                </button>
+              }
+              {block.type ==="todo done" &&
+                <button 
+                  className='checkbox done left blockBtn'
+                  name={block.id}
+                >
+                  <GrCheckboxSelected   
+                    className='blockBtnSvg '
+                  />
+                </button>
+              }
+              {block.type ==="toggle" &&
+                <button 
+                  name={block.id}
+                  className={block.subBlocksId!==null ? 
+                    'blockToggleBtn on left blockBtn' :
+                    'blockToggleBtn left blockBtn' 
+                  }
+                >
+                  <MdPlayArrow 
+                    className='blockBtnSvg'
+                  />
+                </button>
+              }
+              {block.type ==="page" &&
+                <div 
+                  className='pageIcon left'
+                >
+                {block.icon == null?
+                  < GrDocumentText/>
+                :
+                  block.icon
+                }
+                </div>
+              }
+              <div 
+                className='blockContents' 
+                style={blockContentsStyle(block)}
+                onMouseOver={ giveFocusToContent}
+              >
+              <BlockComponent
+                userName={userName} 
+                block={block} 
+                page={page}
+                addBlock={addBlock}
+                editBlock={editBlock}
+                changeToSub={changeToSub}
+                raiseBlock={raiseBlock}
+                deleteBlock={deleteBlock}
+                addPage={addPage}
+                editPage={editPage}
+                deletePage={deletePage}
+                setCommentBlock={setCommentBlock}
+                commentBlock={commentBlock}
+                setTargetPageId={setTargetPageId}
+              />
+              </div>
+              </div>
+              {block.comments !==null &&
+              <BlockComment
+                block={block}
+              />
+              }
+            </div>
+            </>
+            }
+            <div 
+              className='subBlocks'
+            >
+              {subBlocks!==undefined&&
+              subBlocks.map((subBlock :Block)=> 
+                <EditableBlock
+                  key ={subBlocks.indexOf(subBlock)} 
+                  userName={userName} 
+                  page={page}
+                  block={subBlock}
+                  addBlock={addBlock}
+                  editBlock={editBlock}
+                  changeToSub={changeToSub}
+                  raiseBlock={raiseBlock}
+                  deleteBlock={deleteBlock}
+                  addPage={addPage}
+                  editPage={editPage}
+                  deletePage={deletePage}
+                  setCommentBlock={setCommentBlock}
+                  commentBlock={commentBlock}
+                  setTargetPageId={setTargetPageId}
+                />
+              )
+              }
+            </div>
+          </div>
         </div>
       </div>
   )
