45e64ca7b2864a098b5e63ff47a2abc6521a2f9f
BadaHertz52 <lmlwpeace@naver.com>
2022-07-04 21:06:47 +0900

    [DOC] Make #pageMoreFun dom and openPageMorefun state in TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { pathType } from '../containers/NotionRouter';
-import {   Page } from '../modules/notion';
+import {   Block, listItem, Page } from '../modules/notion';
 import {  SideAppear } from '../modules/side';
 
 import { AiOutlineMenu} from 'react-icons/ai';
@@ -9,22 +9,41 @@ import { FiChevronsLeft } from 'react-icons/fi';
 import { AiFillStar, AiOutlineClockCircle, AiOutlineStar } from "react-icons/ai";
 import { BiMessageDetail } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { IoArrowRedoOutline } from 'react-icons/io5';
+import PageMenu from './PageMenu';
 
 type TopBarProps ={
+  firstlist:listItem[],
   favorites:string[]|null,
   sideAppear:SideAppear,
   page:Page,
+  pages:Page[],
   pagePath: pathType[] |null ,
+
+  addBlock:(pageId: string, block: Block, newBlockIndex: number, previousBlockId: string | null) => void,
+  editBlock: (pageId: string, block: Block) => void,
+  deleteBlock: (pageId: string, block: Block, isInMenu: boolean) => void,
+
+  addPage: (newPage: Page) => void
+  deletePage: (pageId: string) => void,
+  movePageToPage: (targetPageId: string, destinationPageId: string) => void,
+
   changeSide: (appear: SideAppear) => void,
+  
   removeFavorites: (itemId: string) => void,
+
   addFavorites: (itemId: string) => void
   setTargetPageId:Dispatch<SetStateAction<string>>,
   showAllComments:boolean,
   setShowAllComments:Dispatch<SetStateAction<boolean>>,
 }
-const TopBar =({favorites,sideAppear,page,pagePath, changeSide ,addFavorites,removeFavorites ,setTargetPageId  , showAllComments, setShowAllComments}:TopBarProps)=>{
+const TopBar =({ firstlist,favorites,sideAppear,page , pages ,pagePath, addBlock, editBlock ,deleteBlock ,addPage,deletePage, movePageToPage, changeSide ,addFavorites,removeFavorites ,setTargetPageId  , showAllComments, setShowAllComments}:TopBarProps)=>{
   const [title, setTitle]= useState<string>("");
+  const [openPageMoreFun, setOpenPageMoreFun] =useState<boolean>(false);
+  const [openPageMenu, setOpenPageMenu]=useState<boolean>(false);
   const pageInFavorites = favorites?.includes(page.id); 
+
   useEffect(()=>{
     if(sideAppear ==="float"){
       setTitle("Lock sideBar open")
@@ -157,9 +176,108 @@ const TopBar =({favorites,sideAppear,page,pagePath, changeSide ,addFavorites,rem
         </button>
         <button
           title=" Style, export, and more"
+          onClick={()=>setOpenPageMoreFun(!openPageMoreFun)}
         >
           <BsThreeDots/>
         </button>
+        {openPageMoreFun &&
+          <div
+            id="pageMoreFun"
+          >
+            <div className='inner'>
+                <div className='fontStyle'>
+                  <div>
+                    STYLE
+                  </div>
+                  <button>
+                    <div>Ag</div>
+                    <div>Default</div>
+                  </button>
+                  <button>
+                    <div>Ag</div>
+                    <div>Serif</div>
+                  </button>
+                  <button>
+                    <div>Ag</div>
+                    <div>Mono</div>
+                  </button>
+                </div>
+                <div className="size">
+                  <button>
+                    <div>Small text</div>
+                    <div className='switchBtn'>
+                      <span className='circleBtn'></span>
+                    </div>
+                  </button>
+                  <button>
+                    <div>Full with</div>
+                    <div className='switchBtn'>
+                      <span className='circleBtn'></span>
+                    </div>
+                  </button>
+                  <div></div>
+                </div>
+                <div className="function">
+                  <button
+                    className={pageInFavorites?"favoriteBtn on" : 
+                    "favoriteBtn"}
+                    onClick={addOrRemoveFavorite}
+                  >
+                    {pageInFavorites ?
+                      <AiFillStar/>
+                      :
+                      <AiOutlineStar/>
+                    }
+                    <span className='label'>
+                    {pageInFavorites ?
+                      "Remove from Favorites"
+                      :
+                      "Add to Favorites"
+                    }
+                    </span>
+                  </button>
+                  <button
+                    onClick={()=>deletePage(page.id)}
+                  >
+                    <RiDeleteBin6Line/>
+                    <span className='label'>
+                      Delete
+                    </span>
+                  </button>
+                  <button
+                    onClick={()=>setOpenPageMenu(!openPageMenu)}
+                  >
+                    <IoArrowRedoOutline/>
+                    <span className='label'>
+                      Move to
+                    </span>
+                  </button>
+                  <button>
+                    <span className='icon'></span>
+                    <div>
+                      <span className='label'>Export</span>
+                      <span>PDF,HTML,Markdown</span>
+                    </div>
+                  </button>
+                </div>
+            </div>
+          </div>
+        }
+        {openPageMenu &&
+        <PageMenu
+          what="page"
+          currentPage={page}
+          firstlist={firstlist}
+          pages={pages}
+          addBlock={addBlock}
+          editBlock={editBlock}
+          deleteBlock={deleteBlock}
+          addPage={addPage}
+          movePageToPage={movePageToPage}
+          setMenuOpen={setOpenPageMenu}
+          setTargetPageId={setTargetPageId}
+        />
+        }
       </div>
     </div>
   )
