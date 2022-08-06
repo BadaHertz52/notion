cb8343ffafa9f353abeb1c1ac8c6e71a90ea4532
BadaHertz52 <lmlwpeace@naver.com>
2022-08-06 18:29:17 +0900

    [DOC,STYLE] Modify Export html to custom select style -Delete select tag instead Make .select_form

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -1,18 +1,40 @@
-import React, { Dispatch, MouseEvent, SetStateAction, useState } from "react";
+import React, { ChangeEvent, Dispatch, MouseEvent, SetStateAction, useState } from "react";
+import { BsWhatsapp } from "react-icons/bs";
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { Page } from "../modules/notion";
 type ExportProps={
   page:Page,
   setOpenExport:Dispatch<SetStateAction<boolean>>,
 }
 const Export =({page,setOpenExport}:ExportProps)=>{
-  const html ="HTMLO";
+  const html ="HTML";
   const pdf="PDF";
-  const markdown="Markdwon";
+  const markdown="Markdown";
   const everything="Everything";
   const noFileImage = 'No files or images';
   type Format = typeof html| typeof pdf | typeof markdown;
   type Content = typeof everything| typeof noFileImage;
-
+  const [format, setFormat]=useState<Format>(html);
+  const [content, setContent]=useState<Content>(everything);
+  const openOptions=(event:MouseEvent)=>{
+    const currentTarget =event.currentTarget;
+    const targetOptions =currentTarget.nextElementSibling;
+    const options =document.querySelectorAll(".options");
+    options.forEach((element:Element)=>{
+      if(element !==targetOptions){
+        element.classList.contains("on")&& element.classList.remove("on")
+      }
+    }) ;
+    currentTarget.nextElementSibling?.classList.toggle("on");
+  };
+  const changeFormat=(event:MouseEvent,FORMAT:Format)=>{
+    event.currentTarget.parentElement?.classList.toggle("on");
+    setFormat(FORMAT);
+  };
+  const changeContent=(event:MouseEvent, CONTENT:Content)=>{
+    event.currentTarget.parentElement?.classList.toggle("on");
+    setContent(CONTENT);  
+  }
   const onClickSwitchBtn=(event:MouseEvent)=>{
     const currentTarget= event.currentTarget;
     const span =currentTarget.querySelector("span");
@@ -21,17 +43,11 @@ const Export =({page,setOpenExport}:ExportProps)=>{
     };
   };
   const onClickExportBtn=()=>{
-    const formatSelect =document.getElementById("formatSelect") as HTMLSelectElement | null;
-    const contentSelect =document.getElementById("contentSelect")  as HTMLSelectElement | null;
     const includeSubpagesSlider =document.getElementById("includeSubPagesSlider");
     const createSubPageFolderSlider =document.getElementById("createSubPageFolderSlider");
-    if(formatSelect !==null &&
-        contentSelect !==null &&
-        includeSubpagesSlider !==null &&
+    if( includeSubpagesSlider !==null &&
         createSubPageFolderSlider !==null 
       ){
-        const format:Format =formatSelect.value as Format;
-        const content:Content =contentSelect.value as Content;
         const includeSubPage :boolean=  includeSubpagesSlider.classList.contains("on");
         const createSubPageFolder :boolean =createSubPageFolderSlider.classList.contains("on");
         console.log("onclick export", format,content, includeSubPage, createSubPageFolder);
@@ -47,30 +63,42 @@ const Export =({page,setOpenExport}:ExportProps)=>{
             <div className="select_label">
                 Export format
             </div>
-            <select id="formatSelect">
-              <option value={html}>
-                {html}
-              </option>
-              <option value={pdf}>
-                {pdf}
-              </option>
-              <option value={markdown}>
-                {markdown}
-              </option>
-            </select>
+            <div className="select_form">
+              <button onClick={(event)=>openOptions(event)}>
+                {format}
+                <MdKeyboardArrowDown/>
+              </button>
+              <div className="options">
+                <button onClick={(event)=>changeFormat(event,html)}>
+                  {html}
+                </button>
+                <button onClick={(event)=>changeFormat(event,pdf)}>
+                  {pdf}
+                </button>
+                <button onClick={(event)=>changeFormat(event,markdown)}>
+                  {markdown}
+                </button>
+              </div>
+            </div>
           </div>
           <div className="select">
             <div className="select_label">
               Include content
             </div>
-            <select id="contentSelect">
-              <option value={everything}>
-                {everything}
-              </option>
-              <option value={noFileImage}>
-                {noFileImage}
-              </option>
-            </select>
+            <div className="select_form">
+              <button onClick={(event)=>openOptions(event)}>
+                {content}
+                <MdKeyboardArrowDown/>
+              </button>
+              <div className="options">
+                <button onClick={(event)=>changeContent(event,everything)}>
+                  {everything}
+                </button>
+                <button onClick={(event)=>changeContent(event,noFileImage)}>
+                  {noFileImage}
+                </button>
+              </div>
+            </div>
           </div>
           <div className="select switch">
             <div className="select_label">
