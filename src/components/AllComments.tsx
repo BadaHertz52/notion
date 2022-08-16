6197c0e2860cd45e22e9516914cc1da094132ae5
BadaHertz52 <lmlwpeace@naver.com>
2022-08-16 17:12:07 +0900

    [FEAT] Modify AllComment to transform it's positoin and change it's width and topBar_lett width , pagePath width   depaning on showAllComment

--- src/components/AllComments.tsx
+++ src/components/AllComments.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { CSSProperties } from "styled-components";
-import { Block, edit_page, Page } from "../modules/notion";
+import { Block, Page } from "../modules/notion";
 import Comments from "./Comments";
 
 type AllCommentsProps={
@@ -19,6 +19,40 @@ const AllComments=({page, userName, editBlock, showAllComments, setShowAllCommen
   const open ="open";
   const resolve="resolve" ;
   const [select, setSelect]=useState<typeof open| typeof resolve>(open);
+
+  const offStyle :CSSProperties ={transform:"translateX(0)"};
+  const [style, setStyle]=useState<CSSProperties>(offStyle);
+
+  const changeStyle=()=>{
+    const innerWidth =window.innerWidth;
+    const topbar_left =document.querySelector(".topbar_left");
+    const pagePath =document.querySelectorAll(".pagePath");
+    const changePathWidth=(topbarLeftWidth:number)=>{
+      const width :number =((topbarLeftWidth -32) / pagePath.length);
+      pagePath.forEach((e:Element)=> e.setAttribute("style",`width:${width}px`));
+    };
+    if(showAllComments){
+      setStyle(offStyle);
+      if(innerWidth >= 385){
+        const newWidth =innerWidth -(12+385+5);
+        topbar_left?.setAttribute("style", `width: ${newWidth}px`);
+        changePathWidth(newWidth);
+        
+      }else{
+        topbar_left!==null &&
+        changePathWidth(topbar_left.clientWidth);
+      }
+    }else{
+      setStyle({transform:`translateX(${innerWidth}px)`});
+      topbar_left?.setAttribute("style", "width:50%");
+      changePathWidth( window.innerWidth -26);
+    }
+  }
+  useEffect(()=>{ 
+    changeStyle();
+  },[showAllComments]);
+  
+  window.onresize= changeStyle;
   const openSelect =(event:React.MouseEvent)=>{
     const target =event.currentTarget;
     const typesDoc = target.parentElement;
@@ -29,11 +63,20 @@ const AllComments=({page, userName, editBlock, showAllComments, setShowAllCommen
     const blocks= page.blocks.filter((block:Block)=> block.comments !==null && block.comments); 
     setTargetCommentsBlocks(blocks);
   },[page]);
-  
+  useEffect(()=>{
+    if(!showAllComments){
+      const onAllComments =document.querySelector(".allComments");
+      onAllComments?.setAttribute("style",  `transform:translateX(${window.innerWidth}px)`
+      )
+    }else{
+      const allComments =document.querySelector(".allComments.on");
+      allComments?.setAttribute("style", "transform:translateX(0)")
+    }
+  },[showAllComments])
   return(
   <div 
     id="allComments"
-    className={showAllComments? "allComments on" : "allComments"}
+    style ={style}
   >
     <div className='inner'>
       <div className='allComments_header'>
