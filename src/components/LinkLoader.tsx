d873d5b05ef499ff739c1565bec6e41c9189aef2
BadaHertz52 <lmlwpeace@naver.com>
2022-10-04 16:30:43 +0900

    [FEAT] Update LinkLoader to add link to block that already has link

--- src/components/LinkLoader.tsx
+++ src/components/LinkLoader.tsx
@@ -18,6 +18,7 @@ type LinkLoaderProps={
   setSelection: Dispatch<SetStateAction<selectionType | null>>,
 }
 const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLink, blockStylerStyle,setSelection}:LinkLoaderProps)=>{
+  const selectedHtml =document.querySelector(".selected");
   const recentPages = recentPagesId!==null? 
                       (recentPagesId.length>3 ?  
                         recentPagesId?.slice(0.4).map((id:string)=> findPage(pagesId,pages,id)) as Page[]
@@ -62,6 +63,11 @@ const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLi
   ".ye",
   ".za",".zm",".zw"
   ];
+  /**
+   * 이미 link 되어 있는 지 여부 
+   */
+  const [linked, setLinked]=useState<boolean>(false);
+  const [linkElement, setLinkElement]=useState<HTMLAnchorElement|NodeListOf<HTMLAnchorElement>|null>(null);
   const [linkLoaderStyle, setLinkLoaderStyle]=useState<CSSProperties|undefined>(undefined);
   const blockStyler =document.getElementById("blockStyler");
   const [searchValue, setSearchValue]= useState<string|null>(null);
@@ -69,7 +75,6 @@ const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLi
   const [webLink, setWebLink]=useState<boolean>(false);
   const onChagneSearch=(event: ChangeEvent<HTMLInputElement>)=>{
     const value = event.target.value;
-
     if(value ===""){
       setSearchValue(null);
       setCandidates(null);
@@ -87,35 +92,91 @@ const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLi
       };
     };
   };
-  const addLink=(link:string)=>{
-    const selectedHtml =document.querySelector(".selected");
-    if(selectedHtml!==null){
-      const newSelectedHtml =document.createElement("a");
-      newSelectedHtml.className="selected link";
-      newSelectedHtml.innerHTML= selectedHtml.innerHTML;
-      newSelectedHtml.setAttribute("target","_blank");
-      if(webLink){
-        newSelectedHtml.setAttribute("href",`${link}`);
-      }else{
-        //page link
-        const originLocation =window.location.origin;
-        const path =`${originLocation}/#/${link}`;
-        newSelectedHtml.setAttribute("href",`${path}`);
-    };
-    selectedHtml.parentNode?.replaceChild(newSelectedHtml,selectedHtml);
+  /**
+   * dom에 변경이 읽을 때,  block의 내용을 담고 있는 element의 innerHTML을 읽어와서, 변경된 내용을 state에 업데이트하는 함수 
+   */
+  const getBlockContents =()=>{
     const targetBlockContentHtml= document.getElementById(`${block.id}_contents`)?.querySelector(".contentEditable");
-    if(targetBlockContentHtml!==null && targetBlockContentHtml!==undefined){
-      const innerHtml =targetBlockContentHtml.innerHTML;
-      const newBlock:Block ={
-        ...block,
-        contents:innerHtml,
-        editTime:JSON.stringify(Date.now())
-      };
-      editBlock(page.id, newBlock);
-      setSelection({block:newBlock});
-    }
-    }
-};
+  if(targetBlockContentHtml!==null && targetBlockContentHtml!==undefined){
+    const innerHtml =targetBlockContentHtml.innerHTML;
+    const newBlock:Block ={
+      ...block,
+      contents:innerHtml,
+      editTime:JSON.stringify(Date.now())
+    };
+    editBlock(page.id, newBlock);
+    setSelection({block:newBlock});
+  };
+  };
+  /**
+   * HTMLAnchorElement의 href 를 변경하는 함수 
+   * @param element 
+   * @param link 
+   */
+  const changeHref =(element:HTMLAnchorElement, link:string)=>{
+    if(webLink){
+      element.setAttribute("href",link);
+    }else{
+      //page link
+      const originLocation =window.location.origin;
+      const path =`${originLocation}/#/${link}`;
+      element.setAttribute("href",`${path}`);
+  };
+  }
+  /**
+   * 새로운  HTMLAnchorElement를 만드는 함수 
+   * @param innerHTML 새로운  HTMLAnchorElement 의 innerHTML의 value
+   * @param link   HTMLAnchorElement 의 href의 value
+   */
+  const makeNewAnchorElement =(innerHTML:string, link:string)=>{
+    const newSelectedHtml =document.createElement("a");
+    newSelectedHtml.className="selected link";
+    newSelectedHtml.setAttribute("target","_blank");
+    newSelectedHtml.innerHTML= innerHTML;
+    changeHref(newSelectedHtml,link);
+    selectedHtml?.parentNode?.replaceChild(newSelectedHtml,selectedHtml);
+    getBlockContents();
+  };
+
+  const addLink=(link:string)=>{
+    if(selectedHtml!==null){
+      if(linked){
+        if(linkElement === selectedHtml){
+          //href 만 변경 
+          changeHref(selectedHtml as HTMLAnchorElement, link);
+          getBlockContents();
+        }else{
+          //기존 link 삭제 후 새로운 link 
+          const selectedHtmlParent = selectedHtml.parentElement;
+          if(linkElement === selectedHtmlParent){
+            changeHref(selectedHtmlParent as HTMLAnchorElement, link);
+            getBlockContents();
+          }else{
+            // 배열 
+            const linkElements =linkElement as NodeListOf<HTMLAnchorElement>; 
+            linkElements.forEach((e:HTMLAnchorElement)=>{
+              console.log("e", e);
+              e.outerHTML =e.innerHTML;
+            });
+            const newSelectedHtml = document.querySelector(
+              ".selected"
+            );
+            console.log("newselectedHtml", newSelectedHtml);
+            newSelectedHtml!==null &&
+            makeNewAnchorElement(newSelectedHtml.innerHTML, link);
+          }
+        }
+      }else{
+        
+        makeNewAnchorElement(selectedHtml.innerHTML, link)
+      }
+    };
+    setOpenLink(false);
+  };
+  const removeLink =()=>{
+
+  };
+
   useEffect(()=>{
     if(blockStyler !==null && blockStylerStyle !==undefined){
       const blockStylerTop =blockStylerStyle.top as string;
@@ -128,6 +189,21 @@ const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLi
       })
     }
   },[ blockStylerStyle,blockStyler]);
+  useEffect(()=>{
+    if(selectedHtml!==null){
+      const selectedHtmlParent =selectedHtml.parentElement;
+      console.log("sp", selectedHtmlParent, selectedHtmlParent?.tagName);
+      const parentLink = selectedHtmlParent?.tagName ==="A" && selectedHtmlParent?.classList.contains("link");
+      const selectedLink = selectedHtml.tagName ==="A" && selectedHtml.classList.contains("link");
+      const haveLinkElement = selectedHtml.querySelector(".link");
+      if(selectedLink|| parentLink||haveLinkElement){
+        setLinked(true);
+        selectedLink && setLinkElement(selectedHtml as HTMLAnchorElement);
+        parentLink && setLinkElement(selectedHtmlParent as HTMLAnchorElement);
+        haveLinkElement && setLinkElement(selectedHtml.querySelectorAll('.link') as NodeListOf<HTMLAnchorElement>);
+      };
+    }
+  },[selectedHtml])
   type PageItemProps={
     page:Page
   };
@@ -206,6 +282,18 @@ const LinkLoader=({recentPagesId, pages,page,pagesId, block,editBlock, setOpenLi
           </span>
         </button>
         }
+        {linked &&
+        <div id="linked">
+          <button>
+            <span>Copy link</span>
+          </button>
+          <button 
+            id="removeLinkBtn"
+            onClick={removeLink}
+          >
+            <span>Remove link</span>
+          </button>
+        </div>}
       </div>
     </div>
   )
