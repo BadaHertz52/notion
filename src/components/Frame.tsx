1e62db2ebfe75be7621fa1bc60e94d01751f880e
BadaHertz52 <lmlwpeace@naver.com>
2022-10-24 18:01:40 +0900

    [DOC] Separate MoveTargetBlock from Frame.tsx to create an independent file

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -22,6 +22,7 @@ import { HiTemplate } from 'react-icons/hi';
 import { setTemplateItem } from './BlockComponent';
 import { fontStyleType } from '../containers/NotionRouter';
 import BlockStyler from './BlockStyler';
+import MoveTargetBlock from './MoveTargetBlock';
 
 
 export type Command ={
@@ -81,73 +82,6 @@ const basicPageCover ='https://raw.githubusercontent.com/BadaHertz52/notion/mast
  * @param param0 
  * @returns 
  */
-const MoveTargetBlock=({ pages,pagesId,page, block , editBlock, addBlock,changeToSub ,raiseBlock, deleteBlock ,smallText, moveBlock  ,setMoveTargetBlock, pointBlockToMoveBlock ,command, setCommand ,setTargetPageId  ,openComment ,setOpenComment ,setCommentBlock ,setOpenLoader, setLoaderTargetBlock, closeMenu,templateHtml ,setSelection
-}:EditableBlockProps)=>{
-  return(
-    <div 
-      id="moveTargetBlock" 
-    >
-      {(block.type.includes("List")&& !block.firstBlock)?
-      (<div className='eidtableBlock'>
-          <div className='editableBlockInner'>
-            <div 
-            id={`moveTarget_block_${block.id}`}
-            className={`${block.type} block`}
-            style={changeFontSizeBySmallText(block, smallText)}
-            >
-              <div  className="mainBlock">
-                <div className='mainBlock_block'>
-                  <div 
-                    id={block.id}
-                    className="blockComponent"
-                  >
-                    <div 
-                      className={`${block.type}_blockComponent blockComponent`}
-                    >
-                      <div 
-                        className='contentEditable'
-                      >
-                        {block.contents !==""? block.contents : "type '/' for commmands"}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-      ):
-      <EditableBlock
-        key={block.id}
-        pages={pages}
-        pagesId={pagesId}
-        page={page}
-        block={block}
-        addBlock={addBlock}
-        editBlock={editBlock}
-        changeToSub={changeToSub}
-        raiseBlock={raiseBlock}
-        deleteBlock={deleteBlock}
-        smallText={smallText}
-        moveBlock={moveBlock}
-        setMoveTargetBlock={setMoveTargetBlock}
-        pointBlockToMoveBlock={pointBlockToMoveBlock}
-        command={command}
-        setCommand={setCommand}
-        setTargetPageId={setTargetPageId}
-        openComment={openComment}
-        setOpenComment={setOpenComment}
-        setCommentBlock={setCommentBlock}
-        setOpenLoader={setOpenLoader}
-        setLoaderTargetBlock={setLoaderTargetBlock}
-        closeMenu={closeMenu}
-        templateHtml={templateHtml}
-        setSelection={setSelection}
-      />
-      }
-    </div>
-  )
-}
 
 const Frame =({ userName,page, pagesId, pages, firstlist ,recentPagesId,editBlock,changeBlockToPage,changePageToBlock, addBlock,changeToSub ,raiseBlock, deleteBlock, addPage, editPage ,duplicatePage,movePageToPage,commentBlock,openComment, setRoutePage ,setTargetPageId ,setOpenComment , setCommentBlock ,
   showAllComments ,smallText , fullWidth  ,discardEdit,setDiscardEdit , openTemplates,  setOpenTemplates, fontStyle}:FrameProps)=>{
