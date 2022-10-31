022f9ae1481d8debf6cb70dea694360b347b8637
BadaHertz52 <lmlwpeace@naver.com>
2022-10-31 20:37:36 +0900

    [DOC] Delete ReactDOM.render instead Use ReactDOM.createRoot  cause update to react18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,17 +9,12 @@ import rootReducer from './modules/index';
 import { Provider } from 'react-redux';
 
 const store =createStore(rootReducer);
-
-ReactDOM.render(
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
