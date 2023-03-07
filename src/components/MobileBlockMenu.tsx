3a7aae0f371802c784828a33d63fd47e0b70c85d
BadaHertz52 <lmlwpeace@naver.com>
2023-03-07 23:58:13 +0900

    [feat] Add code to execute removeSelected function when openSideMenu is false and there exists a node with the 'selected' class

--- src/components/MobileBlockMenu.tsx
+++ src/components/MobileBlockMenu.tsx
@@ -6,7 +6,7 @@ import { TiArrowSortedDown } from 'react-icons/ti';
 import { CSSProperties } from 'styled-components';
 import {  msmWhatType, ms_color, ms_moreMenu, ms_turnInto } from '../containers/NotionRouter';
 import { Block, findBlock, makeNewBlock} from '../modules/notion';
-import BlockStyler, {StylerCommonProps } from './BlockStyler';
+import BlockStyler, {removeSelected, StylerCommonProps } from './BlockStyler';
 
 
 type MobileBlockMenuProps = Omit<StylerCommonProps , 'block'> & {
@@ -171,7 +171,13 @@ useEffect(()=>{
     changeMBMstyle(targetBlock);
     sessionStorage.removeItem('mobileMenuBlock')
   }
- },[item])
+},[item]);
+
+useEffect(()=>{
+  if(!openMobileSideMenu && document.querySelector(".selected") !==null && block !==null){
+    removeSelected(frameHtml, block, editBlock ,page, null)
+  }
+},[openMobileSideMenu])
   return(
     <>
       <div id="mobileBlockMenu" style={mbmStyle}>
