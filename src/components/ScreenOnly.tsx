6798744d3ca72822062dc11cfbd12738314ff089
badahertz52 <badahertz52@gmail.com>
2023-07-18 15:53:37 +0900

    [Fix] Fix errors caused by exporting and using styled components in ScreenOnly.tsx

--- src/components/ScreenOnly.tsx
+++ src/components/ScreenOnly.tsx
@@ -1,20 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
 
 type ScreenOnlyProps = {
   text: string;
 };
 const ScreenOnly = ({ text }: ScreenOnlyProps) => {
-  const ScreenOnlySpan = styled.span`
-    position: absolute !important;
-    clip: rect(1px, 1px, 1px, 1px);
-    padding: 0 !important;
-    border: 0 !important;
-    height: 1px !important;
-    width: 1px !important;
-    overflow: hidden;
-  `;
-  return <ScreenOnlySpan>{text}</ScreenOnlySpan>;
+  const style: CSSProperties = {
+    position: "absolute",
+    clip: "rect(1px, 1px, 1px, 1px)",
+    padding: "0 ",
+    border: "0 ",
+    height: "1px ",
+    width: "1px ",
+    overflow: "hidden",
+  };
+
+  return (
+    <span className="screenOnly" style={style}>
+      {text}
+    </span>
+  );
 };
 
 export default React.memo(ScreenOnly);
