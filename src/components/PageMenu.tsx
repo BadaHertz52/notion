7f55756b90f96b8da913e55dcc3e6fe803648c06
badahertz52 <badahertz52@gmail.com>
2023-07-22 16:49:07 +0900

    [Refactor] Optimaze PageMenu

--- src/components/PageMenu.tsx
+++ src/components/PageMenu.tsx
@@ -5,6 +5,7 @@ import React, {
   useEffect,
   useState,
   useContext,
+  useCallback,
 } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { ActionContext } from "../containers/NotionRouter";
@@ -39,30 +40,33 @@ const PageMenu = ({
 
   const templateHtml = document.getElementById("template");
 
-  const findResult = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(true);
-    const value = event.target.value;
-    const filteredPages: Page[] = pages.filter((page: Page) =>
-      page.header.title?.includes(value)
-    );
-    const RESULT: listItem[] = filteredPages.map((page: Page) => ({
-      id: page.id,
-      title: page.header.title,
-      iconType: page.header.iconType,
-      icon: page.header.icon,
-      subPagesId: page.subPagesId,
-      parentsId: page.parentsId,
-      editTime: JSON.stringify(Date.now()),
-      createTime: JSON.stringify(Date.now()),
-    }));
-    setResult(RESULT);
-  };
-  const makeNewSubPage = () => {
+  const findResult = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      setSearch(true);
+      const value = event.target.value;
+      const filteredPages: Page[] = pages.filter((page: Page) =>
+        page.header.title?.includes(value)
+      );
+      const RESULT: ListItem[] = filteredPages.map((page: Page) => ({
+        id: page.id,
+        title: page.header.title,
+        iconType: page.header.iconType,
+        icon: page.header.icon,
+        subPagesId: page.subPagesId,
+        parentsId: page.parentsId,
+        editTime: JSON.stringify(Date.now()),
+        createTime: JSON.stringify(Date.now()),
+      }));
+      setResult(RESULT);
+    },
+    [pages]
+  );
+  const makeNewSubPage = useCallback(() => {
     if (block) {
       setTemplateItem(templateHtml, currentPage);
       changeBlockToPage(currentPage.id, block);
     }
-  };
+  }, [block, changeBlockToPage, currentPage, templateHtml]);
   useEffect(() => {
     if (sessionItem && what === "block") {
       const block: Block = JSON.parse(sessionItem);
