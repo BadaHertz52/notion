6ccf077c3728a726a0e4cd9597d6f113bb911151
badahertz52 <badahertz52@gmail.com>
2023-08-01 17:24:54 +0900

    [style] Modify style code
     -Modify value of  itemSvg
    -Add texxt-decoration to todo_done.block
    -Delete style from .page__icon__img

--- src/components/PageIcon.tsx
+++ src/components/PageIcon.tsx
@@ -19,12 +19,7 @@ const PageIcon = ({ icon, iconType, style }: PageItemProps) => {
   return (
     <div className={pageIconClassName} style={style}>
       <span>
-        <img
-          className="page__icon__img"
-          alt="pageImgIcon"
-          style={{ maxWidth: "100px", maxHeight: "100px" }}
-          src={imgSrc}
-        />
+        <img className="page__icon__img" alt="pageImgIcon" src={imgSrc} />
       </span>
     </div>
   );
