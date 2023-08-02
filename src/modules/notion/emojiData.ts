7d26b565218fe1eb4893ef370607f3b0f5f2c236
badahertz52 <badahertz52@gmail.com>
2023-08-02 20:40:11 +0900

    Delete unneeded

--- src/modules/notion/emojiData.ts
+++ src/modules/notion/emojiData.ts
@@ -94,36 +94,27 @@ export const getEmojiUrl = (
   icon: Emoji,
   setUrl: Dispatch<SetStateAction<string | undefined>>
 ) => {
-  // 캔버스 요소와 컨텍스트 객체를 생성합니다.
   let canvas = document.createElement("canvas");
   let ctx = canvas.getContext("2d", { alpha: true });
-
-  // Image 객체를 생성하고 src 속성에 데이터 URL을 할당합니다.
   let image = new Image();
   image.src = EmojiSpritesImg;
-  // onload 이벤트 핸들러를 추가합니다.
   image.onload = function () {
-    // 캔버스의 크기를 잘라낼 영역의 크기와 같게 설정합니다.
     const size = 240;
-    canvas.width = size; // 잘라낼 영역의 너비
-    canvas.height = size; // 잘라낼 영역의 높이
-    // 캔버스에 이미지의 일부분을 잘라서 그립니다.
+    canvas.width = size;
+    canvas.height = size;
     ctx?.clearRect(0, 0, canvas.width, canvas.height);
     ctx?.drawImage(
       image,
-      EMOJI_ARR.indexOf(icon) * (size + 10), // 잘라낼 영역의 x 좌표
-      0, // 잘라낼 영역의 y 좌표
-      size, // 잘라낼 영역의 너비
-      size, // 잘라낼 영역의 높이
-      0, // 캔버스에 그릴 영역의 x 좌표
-      0, // 캔버스에 그릴 영역의 y 좌표
-      size, // 캔버스에 그릴 영역의 너비
-      size // 캔버스에 그릴 영역의 높이
+      EMOJI_ARR.indexOf(icon) * (size + 10),
+      0,
+      size,
+      size,
+      0,
+      0,
+      size,
+      size
     );
     setUrl(canvas.toDataURL());
     canvas.remove();
   };
 };
-
-export const emojiPath =
-  "https://raw.githubusercontent.com/BadaHertz52/notion/master/image/emoji/";
