8e215fc4a3ce3b53921aa52764ee4fcc7df442c1
badahertz52 <badahertz52@gmail.com>
2023-08-16 17:25:09 +0900

    Recovery ListTemplate.tsx
    - ListTemplate에 react-virtalize 사용 시, list가 보이지 않는 오류가 해결되지 않아 원래대로 되돌림

--- src/components/ListTemplate.tsx
+++ src/components/ListTemplate.tsx
@@ -1,26 +1,12 @@
-import React, {
-  useContext,
-  useCallback,
-  useRef,
-  useState,
-  useEffect,
-} from "react";
+import React, { useContext, useCallback, useRef } from "react";
 import { ListItem, Notion, Page } from "../modules/notion/type";
 import { SideAppear } from "../modules/side/reducer";
 import { ActionContext } from "../route/NotionRouter";
 import ItemTemplate from "./ItemTemplate";
-import {
-  AutoSizer,
-  CellMeasurer,
-  CellMeasurerCache,
-  List,
-  ListRowProps,
-  WindowScroller,
-} from "react-virtualized";
 
 type ListTemplateProp = {
   notion: Notion;
-  targetList: ListItem[] | null;
+  targetList: ListItem[];
   onClickMoreBtn: (item: ListItem, target: HTMLElement) => void;
   addNewSubPage: (item: ListItem) => void;
   changeSide: (appear: SideAppear) => void;
@@ -34,9 +20,6 @@ const ListTemplate = ({
 }: ListTemplateProp) => {
   const { changeSide } = useContext(ActionContext).actions;
   const ulRef = useRef<HTMLUListElement>(null);
-  const [height, setHeight] = useState<number>(
-    window.innerWidth > 768 ? 32 : 36
-  );
   const findSubPage = (
     id: string,
     pagesId: string[],
@@ -64,90 +47,41 @@ const ListTemplate = ({
     },
     []
   );
-  const cache = new CellMeasurerCache({
-    defaultWidth: ulRef.current?.offsetWidth,
-    fixedWidth: true,
-  });
-  const Row = ({ index, measure }: { index: number; measure: () => void }) => {
-    const item = (targetList as ListItem[])[index];
-    return (
-      <li key={item.id}>
-        <div className="mainPage">
-          <ItemTemplate
-            item={item}
-            onClickMoreBtn={onClickMoreBtn}
-            addNewSubPage={addNewSubPage}
-            changeSide={changeSide}
-            handleImgLoad={measure}
-          />
-        </div>
-        {notion.pages &&
-          notion.pagesId &&
-          (item.subPagesId ? (
-            <div className="subPage">
-              <ListTemplate
-                notion={notion}
-                targetList={makeTargetList(
-                  item.subPagesId,
-                  notion.pagesId,
-                  notion.pages
-                )}
-                onClickMoreBtn={onClickMoreBtn}
-                addNewSubPage={addNewSubPage}
-                changeSide={changeSide}
-              />
-            </div>
-          ) : (
-            <div className="subPage no">
-              <span>No page inside</span>
-            </div>
-          ))}
-      </li>
-    );
-  };
-  const rowRender = (rowRenderProps: ListRowProps) => {
-    const { parent, key, index } = rowRenderProps;
-    return (
-      <CellMeasurer
-        cache={cache}
-        parent={parent}
-        key={key}
-        columnIndex={0}
-        rowIndex={index}
-      >
-        {({ measure }) => <Row index={index} measure={measure} />}
-      </CellMeasurer>
-    );
-  };
-  const changeHeight = () => {
-    setHeight(window.innerWidth > 768 ? 32 : 36);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", changeHeight);
-    return window.removeEventListener("resize", changeHeight);
-  }, []);
   return (
     <ul ref={ulRef}>
-      {targetList && (
-        <WindowScroller>
-          {() => (
-            <AutoSizer style={{ height: height }}>
-              {({ width, height }) => {
-                <List
-                  autoHeight
-                  height={height}
-                  width={width}
-                  overscanRowCount={0}
-                  rowCount={targetList.length}
-                  rowHeight={cache.rowHeight}
-                  rowRenderer={rowRender}
-                  deferredMeasurementCache={cache}
-                />;
-              }}
-            </AutoSizer>
-          )}
-        </WindowScroller>
-      )}
+      {targetList.map((item) => (
+        <li key={item.id}>
+          <div className="mainPage">
+            <ItemTemplate
+              item={item}
+              onClickMoreBtn={onClickMoreBtn}
+              addNewSubPage={addNewSubPage}
+              changeSide={changeSide}
+            />
+          </div>
+          {notion.pages &&
+            notion.pagesId &&
+            (item.subPagesId ? (
+              <div className="subPage">
+                <ListTemplate
+                  notion={notion}
+                  targetList={makeTargetList(
+                    item.subPagesId,
+                    notion.pagesId,
+                    notion.pages
+                  )}
+                  onClickMoreBtn={onClickMoreBtn}
+                  addNewSubPage={addNewSubPage}
+                  changeSide={changeSide}
+                />
+              </div>
+            ) : (
+              <div className="subPage no">
+                <span>No page inside</span>
+              </div>
+            ))}
+        </li>
+      ))}
     </ul>
   );
 };
