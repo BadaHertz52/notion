569c4faa131f087e91ee88fa5849ed56a0711f72
badahertz52 <badahertz52@gmail.com>
2023-08-20 21:17:43 +0900

    [Feat] Add code that set vh whenever window resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NotionRouter from "./route/NotionRouter";
 import "./assets/main.scss";
-function App() {
+const App = () => {
+  const setVh = () => {
+    document.documentElement.style.setProperty(
+      "--vh",
+      `${window.innerHeight}px`
+    );
+  };
+  useEffect(() => {
+    window.addEventListener("resize", setVh);
+    setVh();
+    return () => window.removeEventListener("resize", setVh);
+  });
+
   return <NotionRouter />;
-}
+};
 
 export default App;
