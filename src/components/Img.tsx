be8693e71472d25373b99619516d096d64e127b7
badahertz52 <badahertz52@gmail.com>
2023-08-22 00:56:47 +0900

    Modify Img component to delete unneed code and arrange code

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -12,37 +12,21 @@ type ImgProps = {
 const Img = (props: ImgProps) => {
   const isGif = props.src.includes("gif");
   const item = localStorage.getItem("web_able");
-  const webAble = item ? item === "true" : false;
+  const webPAble = item === "true";
   const [jpegSrc, setJpegSrc] = useState<string>("");
-
   useEffect(() => {
-    if (!webAble && !isGif) {
+    if (!webPAble && !isGif) {
       changeImgToJpeg(props.src, (url: string) => setJpegSrc(url));
     }
-  }, [webAble, isGif, setJpegSrc, props.src]);
+  }, [webPAble, isGif, setJpegSrc, props.src]);
   return (
-    <picture>
-      {isGif ? (
-        <img
-          className={props.className}
-          style={props.style}
-          src={props.src}
-          alt={props.alt}
-        />
-      ) : (
-        <>
-          <source srcSet={props.src} type="image/webp" />
-          <source srcSet={jpegSrc} type="image/jpeg" />
-          <img
-            className={props.className}
-            style={props.style}
-            src={props.src}
-            alt={props.alt}
-          />
-        </>
-      )}
-    </picture>
+    <img
+      className={props.className}
+      style={props.style}
+      src={jpegSrc ? jpegSrc : props.src}
+      alt={props.alt}
+    />
   );
 };
 
-export default Img;
+export default React.memo(Img);
