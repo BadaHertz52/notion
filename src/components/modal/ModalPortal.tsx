56696f560fd1306400bda2a99bccaad4ef660923
badahertz52 <badahertz52@gmail.com>
2023-12-09 18:27:39 +0900

    docs<ModalPortal> : touchMove 프로퍼티 추가

--- src/components/modal/ModalPortal.tsx
+++ src/components/modal/ModalPortal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, CSSProperties } from "react";
+import React, { ReactNode, CSSProperties, TouchEvent } from "react";
 import * as ReactDOM from "react-dom";
 
 import "../../assets/modal.scss";
@@ -8,11 +8,22 @@ type ModalPortalProps = {
   isOpen: boolean;
   children: ReactNode;
   style?: CSSProperties;
+  onTouchMove?: (event: TouchEvent<HTMLDivElement>) => void;
 };
-const ModalPortal = ({ id, isOpen, children, style }: ModalPortalProps) => {
+const ModalPortal = ({
+  id,
+  isOpen,
+  children,
+  style,
+  onTouchMove,
+}: ModalPortalProps) => {
   const modalRootEl = document.getElementById("modal-root") as HTMLElement;
   return ReactDOM.createPortal(
-    <div id={id} className={`modal ${isOpen ? "on" : ""}`}>
+    <div
+      id={id}
+      className={`modal ${isOpen ? "on" : ""}`}
+      onTouchMove={onTouchMove}
+    >
       <div className="inner">
         <div id={`${id}__menu`} style={style}>
           {children}
